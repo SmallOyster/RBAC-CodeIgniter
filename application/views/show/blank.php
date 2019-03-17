517d6db6360da150172c193663d0c2edfc6667c8
Jerry Cheung <SmallOyster@users.noreply.github.com>
2019-03-17 22:14:08 +0800

    build 00501: add jumpOut&example page
    + 示例空白页
    + 自动跳出站外页

--- application/views/show/blank.php
+++ application/views/show/blank.php
@@ -1,36 +1,40 @@
 <?php 
 /**
- * @name V-空白页
+ * @name 生蚝科技RBAC开发框架-V-空白示例页
  * @author Jerry Cheung <master@xshgzs.com>
  * @since 2018-03-15
- * @version V1.0 2018-08-07
+ * @version 2019-03-17
  */ 
 ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-  <?php $this->load->view('include/header'); ?>
-  <title>空白页 / <?=$this->Setting_model->get('systemName');?></title>
+	<?php $this->load->view('include/header'); ?>
+	<title><?=$this->setting->get('systemName'); ?></title>
 </head>
 
-<body>
-<div id="wrapper">
+<body class="hold-transition skin-cyan sidebar-mini">
+<div class="wrapper">
 
 <?php $this->load->view('include/navbar'); ?>
 
-<!-- Page Content -->
-<div id="page-wrapper">
+<!-- 页面内容 -->
+<div id="app" class="content-wrapper">
+	<?php $this->load->view('include/pagePath',['name'=>'空白示例页','path'=>[['空白示例页','',1]]]); ?>
 
-<!-- Page Name-->
-<div class="row">
-	<div class="col-lg-12">
-		<h1 class="page-header">空白页</h1>
-	</div>
+	<!-- 页面主要内容 -->
+	<section class="content">
+		<div class="box">
+			<div class="box-body">
+				在此显示文字或其他内容
+			</div>
+		</div>
+ 	</section>
+	<!-- ./页面主要内容 -->
 </div>
-<!-- ./Page Name-->
-  
+<!-- ./页面内容 -->
+
 <?php $this->load->view('include/footer'); ?>
 
 <!-- ./Page Main Content -->
--- application/views/show/jumpOut.php
+++ application/views/show/jumpOut.php
@@ -1,33 +1,53 @@
+<?php 
+/**
+ * @name 生蚝科技RBAC开发框架-V-页面跳转
+ * @author Jerry Cheung <master@xshgzs.com>
+ * @since 2018-03-15
+ * @version 2019-03-17
+ */ 
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
-  <?php $this->load->view('include/header'); ?>
-  <title><?=$this->Setting_model->get('systemName');?></title>
+	<?php $this->load->view('include/header'); ?>
+	<title><?=$this->setting->get('systemName'); ?></title>
 </head>
 
-<body>
-<div id="wrapper">
+<body class="hold-transition skin-cyan sidebar-mini">
+<div class="wrapper">
 
 <?php $this->load->view('include/navbar'); ?>
 
-<div id="page-wrapper">
-<!-- Page Content -->
+<!-- 页面内容 -->
+<div id="app" class="content-wrapper">
+	<?php $this->load->view('include/pagePath',['name'=>($name!=""?$name:"页面跳转"),'path'=>[['页面跳转','',1]]]); ?>
 
-<div class="row">
-  <div class="col-lg-12">
-    <h1 class="page-header"><?=$name!=""?$name:"页面跳转";?></h1>
-  </div>
+	<!-- 页面主要内容 -->
+	<section class="content">
+		<a href="<?=$url;?>" target="_blank" class="btn btn-success btn-block" style="font-size:20px;font-weight:bold;"><i class="fa fa-link" aria-hidden="true"></i> 点 此 手 动 跳 转</a>
+		<br>
+		<a onclick="history.go(-1);" class="btn btn-primary btn-block" style="font-size:20px;font-weight:bold;"><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i> 返 回 上 一 页</a>
+	</section>
+	<!-- ./页面主要内容 -->
 </div>
-
-<a href="<?=$url;?>" target="_blank" class="btn btn-success" style="width: 98%">请 点 此 手 动 跳 转</a>
-<br><br>
-<a onclick="history.go(-1);" class="btn btn-primary" style="width: 98%">返 回 上 一 页</a>
+<!-- ./页面内容 -->
 
 <?php $this->load->view('include/footer'); ?>
 
-<!-- ./Page Content -->
+<!-- ./Page Main Content -->
 </div>
+<!-- ./Page -->
 </div>
+
+<script>
+var url="<?=$url;?>";
+lockScreen("跳转中，请稍候...");
+setTimeout(function(){
+	window.open(url);
+	unlockScreen();
+	history.go(-1);
+},2000);
+</script>
 </body>
 </html>
