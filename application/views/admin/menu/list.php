4d7df44b0cb08eb1ba2fca324d1b594ab6a24691
Jerry Cheung <SmallOyster@users.noreply.github.com>
2019-05-18 22:07:20 +0800

    build 00600: change tree plugin
    ~ 菜单管理改用zTree显示

--- application/views/admin/menu/list.php
+++ application/views/admin/menu/list.php
@@ -3,7 +3,7 @@
  * @name 生蚝科技RBAC开发框架-V-菜单管理
  * @author Jerry Cheung <master@xshgzs.com>
  * @since 2018-02-17
- * @version 2019-03-22
+ * @version 2019-05-18
  */
 ?>
 <!DOCTYPE html>
@@ -11,7 +11,11 @@
 
 <head>
 	<?php $this->load->view('include/header'); ?>
-	<title>菜单管理 / <?=$this->Setting_model->get('systemName');?></title>
+	<title>菜单管理 / <?=$this->setting->get('systemName');?></title>
+	<style>
+		.ztree li span.button.add {margin-left:2px; margin-right: -1px; background-position:-144px 0; vertical-align:top; *vertical-align:middle}
+		ul.ztree {margin-top: 10px;border: 1px solid #617775;background: #f0f6e4;height:360px;overflow-y:scroll;overflow-x:auto;}
+	</style>
 </head>
 
 <body class="hold-transition skin-cyan sidebar-mini">
@@ -26,33 +30,8 @@
 	<!-- 页面主要内容 -->
 	<section class="content">
 		<a href="<?=base_url('admin/menu/add/0'); ?>" class="btn btn-primary btn-block">新 增 主 菜 单</a>
-		<table class="table table-hover" style="border-radius: 5px; border-collapse: separate;">
-			<thead>
-				<tr>
-					<th>菜单名</th>
-					<th>操作</th>
-				</tr>
-			</thead>
 
-			<tbody>
-			<?php foreach($list as $info){ ?>
-				<tr>
-					<td><i class="fa fa-<?=$info['icon']; ?>" aria-hidden="true"></i> <?=$info['name']; ?></td>
-					<td><a href="<?=base_url('admin/menu/edit/').$info['id']; ?>" class="btn btn-info">编辑</a> <button onclick='del_ready("<?=$info['id']; ?>","<?=$info['name']; ?>")' class="btn btn-danger">删除</button> <a href="<?=base_url('admin/menu/add/').$info['id']; ?>" class="btn btn-success">新增子菜单</a></td>
-				</tr>
-				<?php foreach($info['child'] as $child_info){ ?>
-				<tr>
-					<td>---- <i class="fa fa-<?=$child_info['icon']; ?>" aria-hidden="true"></i> <?=$child_info['name']; ?></td>
-					<td><a href="<?=base_url('admin/menu/edit/').$child_info['id']; ?>" class="btn btn-info">编辑</a> <button onclick='del_ready("<?=$child_info['id']; ?>","<?=$child_info['name']; ?>")' class="btn btn-danger">删除</button> <a href="<?=base_url('admin/menu/add/').$child_info['id']; ?>" class="btn btn-success">新增子菜单</a></td>
-				</tr>
-				<?php foreach($child_info['child'] as $child2_info){ ?>
-				<tr>
-					<td>-------- <i class="fa fa-<?=$child2_info['icon']; ?>" aria-hidden="true"></i> <?=$child2_info['name']; ?></td>
-					<td><a href="<?=base_url('admin/menu/edit/').$child2_info['id']; ?>" class="btn btn-info">编辑</a> <button onclick='del_ready("<?=$child2_info['id']; ?>","<?=$child2_info['name']; ?>")' class="btn btn-danger">删除</button></td>
-				</tr>
-				<?php } } } ?>
-			</tbody>
-		</table>
+		<ul id="treeDemo" class="ztree"></ul>
 	</section>
 	<!-- ./页面主要内容 -->
 </div>
@@ -66,6 +45,72 @@
 </div>
 
 <script>
+var setting = {
+	view: {
+		selectedMulti: false
+	},
+	data: {
+		simpleData: {
+			enable: true
+		}
+	},
+	view: {
+		addHoverDom: addHoverDom,
+		removeHoverDom: removeHoverDom,
+	}
+};
+
+var zNodes=getAllMenu();
+
+$(document).ready(function(){
+	$.fn.zTree.init($("#treeDemo"),setting,zNodes);
+});
+function addHoverDom(treeId, treeNode) {
+	var aObj=$("#"+treeNode.tId+"_a");
+	var editStr=""
+		+"<button class='btn btn-info' id='treeBtn_edit_"+treeNode.id+"' onclick='window.location.href="+'"'+"<?=base_url('admin/menu/edit/');?>"+treeNode.id+'"'+"'>编辑</button> "
+		+"<button class='btn btn-danger' id='treeBtn_delete_"+treeNode.id+"' onclick="+'"'+"del_ready("+treeNode.id+",'"+treeNode.name+"')"+'"'+"'>删除</button> "
+		+"<button class='btn btn-success' id='treeBtn_add_"+treeNode.id+"' onclick='window.location.href="+'"'+"<?=base_url('admin/menu/add/');?>"+treeNode.id+'"'+"'>新增子菜单</button>";
+	
+	// 如果已存在button就返回
+	if($("#treeBtn_edit_"+treeNode.id).length>0) return;
+	if($("#treeBtn_delete_"+treeNode.id).length>0) return;
+	if($("#treeBtn_add_"+treeNode.id).length>0) return;
+	
+	aObj.append(editStr);
+	
+	// 三级菜单不允许新增子菜单
+	if(treeNode.level>=2) $("#treeBtn_add_"+treeNode.id).remove();
+}
+function removeHoverDom(treeId, treeNode) {
+	$("#treeBtn_edit_"+treeNode.id).unbind().remove();
+	$("#treeBtn_delete_"+treeNode.id).unbind().remove();
+	$("#treeBtn_add_"+treeNode.id).unbind().remove();
+}
+
+function getAllMenu(){
+	roleId=$("#roleId").val();
+	
+	$.ajax({
+		url:"<?=base_url('api/role/getRoleMenuForZtree/0');?>",
+		dataType:"json",
+		async:false,
+		error:function(e){
+			console.log(e);
+			unlockScreen();
+			showModalTips("服务器错误！<hr>请联系技术支持并提交以下错误码：<br><font color='blue'>"+e.status+"</font>");
+
+			$("#tipsModal").modal('show');
+			return false;
+		},
+		success:function(got){
+			ret=got;
+		}
+	});
+	return ret;
+}
+
+
 function del_ready(id,name){
 	$("#delId").val(id);
 	$("#delName_show").html(id+". "+name);
@@ -78,10 +123,10 @@ function del_sure(){
 	id=$("#delId").val();
 
 	$.ajax({
-		url:"<?=base_url('admin/menu/toDelete'); ?>",
+		url:"<?=base_url('admin/menu/toDelete');?>",
 		type:"post",
 		dataType:"json",
-		data:{<?=$this->ajax->showAjaxToken(); ?>,"id":id},
+		data:{<?=$this->ajax->showAjaxToken();?>,"id":id},
 		error:function(e){
 			console.log(e);
 			unlockScreen();
