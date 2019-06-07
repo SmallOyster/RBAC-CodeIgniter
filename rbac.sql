86d90fb8ee1336117491fe05e6ee01d29bb2ab62
Jerry Cheung <SmallOyster@users.noreply.github.com>
2019-06-07 09:15:51 +0800

    build 00667: remove notice database
    - 通知模块数据库

--- rbac.sql
+++ rbac.sql
@@ -26,30 +26,15 @@ INSERT INTO `menu` (`id`, `father_id`, `name`, `icon`, `uri`, `create_time`, `up
 (3, 1, '角色列表', 'users', 'admin/role/list', '2018-02-18 04:46:23', ''),
 (4, 1, '菜单管理', 'bars', 'admin/menu/list', '2018-02-18 04:46:23', ''),
 (5, 1, '操作记录列表', 'list-alt', 'admin/sys/log/list', '2018-02-18 04:46:23', ''),
-(7, 1, '公告管理', 'bell', '', '2018-02-18 04:46:23', '2018-03-29 22:02:37'),
 (8, 1, '修改系统参数', 'gear', 'admin/sys/setting/list', '2018-03-01 21:16:51', '2019-03-17 22:30:14'),
 (10, 0, '示例页面', 'file', '', '2018-03-14 06:39:43', '2018-03-14 22:42:10'),
 (11, 10, '列表页', 'list', 'show/list', '2018-03-14 06:41:24', '0000-00-00 00:00:00'),
 (12, 10, '空白页', 'file-o', 'show/blank', '2018-03-14 06:41:59', '0000-00-00 00:00:00'),
 (13, 10, '表单页', 'table', 'show/form', '2018-03-14 06:43:46', '0000-00-00 00:00:00'),
-(14, 7, '公告列表', 'list-alt', 'admin/notice/list', '2018-03-14 21:08:11', '2018-03-28 18:14:03'),
-(15, 7, '发布新公告', 'bullhorn', 'admin/notice/pub', '2018-03-14 21:08:46', '2019-03-17 19:26:20'),
 (17, 0, '常用链接', 'link', '', '2019-03-23 15:49:14', '0000-00-00 00:00:00'),
 (18, 17, '图标库', 'circle-o', 'show/jumpout/http%3A%2F%2Fwww.fontawesome.com.cn%2Ffaicons%2F', '2019-03-23 15:53:45', '0000-00-00 00:00:00'),
 (19, 17, '数据库管理', 'circle-o', 'show/jumpout/https%3A%2F%2Fwww.xshgzs.com%2FdbAdmin%2F', '2019-03-24 00:20:50', '0000-00-00 00:00:00'),
 (20, 17, 'SSO用户中心', 'circle-o', 'show/jumpout/https%3A%2F%2Fssouc.xshgzs.com', '2019-03-24 00:22:09', '0000-00-00 00:00:00');
-
-CREATE TABLE `notice` (
-  `id` int(11) NOT NULL,
-  `title` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `content` text COLLATE utf8_unicode_ci NOT NULL,
-  `receiver` text COLLATE utf8_unicode_ci NOT NULL,
-  `publisher_id` int(11) NOT NULL,
-  `praise` int(11) NOT NULL DEFAULT '0',
-  `read_count` int(11) NOT NULL DEFAULT '0',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `role` (
   `id` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
@@ -75,9 +60,6 @@ INSERT INTO `role_permission` (`id`, `role_id`, `menu_id`) VALUES
 (3, '1p2wx4', 3),
 (4, '1p2wx4', 4),
 (5, '1p2wx4', 5),
-(6, '1p2wx4', 7),
-(7, '1p2wx4', 14),
-(8, '1p2wx4', 15),
 (9, '1p2wx4', 8),
 (10, '1p2wx4', 10),
 (11, '1p2wx4', 11),
@@ -125,7 +107,7 @@ CREATE TABLE `setting` (
 
 INSERT INTO `setting` (`id`, `name`, `chinese_name`, `value`, `create_time`, `update_time`) VALUES
 (1, 'sessionPrefix', 'Session名称前缀', 'OTRBAC2_', '2018-03-05 03:55:19', '2018-09-26 18:45:52'),
-(2, 'systemName', '系统名称', '.0', '2018-03-05 03:55:19', '2019-05-17 22:13:13'),
+(2, 'systemName', '系统名称', '生蚝科技RBAC开发框架V2.0', '2018-03-05 03:55:19', '2019-05-17 22:13:13'),
 (3, 'apiPath', 'API接口目录', 'https://test.xshgzs.com/rbac/api/', '2019-02-23 09:29:55', '2019-03-17 22:20:08'),
 (4, 'ssoAppId', 'SSO-AppId', '', '2019-03-17 14:22:25', '2019-03-17 22:28:51'),
 (5, 'ssoReturnUrl', 'SSO-回调URL', 'https://test.xshgzs.com/rbac/SSO/login', '2019-03-17 14:22:56', '0000-00-00 00:00:00'),
@@ -158,9 +140,6 @@ ALTER TABLE `log`
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `notice`
-  ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `role`
   ADD UNIQUE KEY `name` (`name`),
   ADD UNIQUE KEY `id` (`id`);
@@ -186,8 +165,6 @@ ALTER TABLE `log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-ALTER TABLE `notice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `role_permission`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 ALTER TABLE `send_mail`
